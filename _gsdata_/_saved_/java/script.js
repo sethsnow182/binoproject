e17a294c7bf6fc10f84113d8c5a07ce399b1460a
Adrian Kula <contact@eview.pl>
2019-11-08 21:51:12 +0100

    Initial commit

---
+++ _gsdata_/_saved_/java/script.js
@@ -0,0 +1,6 @@
+/* NAVIGATION */
+
+    // Open navigation
+    $("#open_nav").on("click", function () { // When button is clicked
+        $("#navigation").toggleClass("active");  // Toggle active class of #navigation
+});
